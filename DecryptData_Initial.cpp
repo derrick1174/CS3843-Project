368d0ffac028e787444169e781bfcf8b30878ed7
derrick1174 <31445009+derrick1174@users.noreply.github.com>
2018-03-08 14:07:06 -0600

    Adds my initial attempt at the code of milestone1

---
+++ DecryptData_Initial.cpp
@@ -0,0 +1,40 @@
+// DecryptData.cpp
+//
+// THis file uses the input data and key information to decrypt the input data
+//
+
+#include "Main.h"
+
+//////////////////////////////////////////////////////////////////////////////////////////////////
+// code to decrypt the data as specified by the project assignment
+int decryptData(char *data, int dataLength)
+{
+	int resulti = 0;
+
+	gdebug1 = 0;					// a couple of global variables that could be used for debugging
+	gdebug2 = 0;					// also can have a breakpoint in C code
+
+	// You can not declare any local variables in C, but should use resulti to indicate any errors
+	// Set up the stack frame and assign variables in assembly if you need to do so
+	// access the parameters BEFORE setting up your own stack frame
+	// Also, you cannot use a lot of global variables - work with registers
+
+	__asm {
+		//This segment of code will perform the same as the following in C: for(i=0;i<datalength;i++); *(data+i) = *(data+i) ^ 1; //The carrot here means XOR
+		XOR ecx,ecx 	//sets ecx to zero
+		mov edx,data	//moves the file data into edx
+		
+		start: 
+			edx+ecx
+			cmp ecx,datalength
+			je done
+			inc ecx
+			jmp start
+		done:
+			mov data,edx
+			mov resulti,ecx
+	}
+
+	return resulti;
+} // decryptData
+
